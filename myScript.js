ced48faa5933edf8fe3c467232e6aa544ae5d25b
Paulina <pau.salinass@hotmail.com>
2020-11-13 15:07:13 -0500

    initial project part 1

---
+++ myScript.js
@@ -0,0 +1,15 @@
+// Welcome Memory Game
+// Prompt username request
+// Cards are to be shuffled on load or restart
+// User will select one tile(listen for that button)
+    //  Tile will flip and show image
+// User selects second tile
+// Tile will flip and show image
+// If they match, tile disappear.
+    //  If they do not match, tiles will flip back. 
+    //Prevent users of clicking more than 2 tiles
+//  Once all the tiles are match the game is over. 
+// Prompt “Congrats you completed the game!” message
+// Restart button button should allo the player reset the game board. 
+// 
+
