0b97a758864a45c92c850b8ccf5c57b15360b4d2
Paulina <pau.salinass@hotmail.com>
2020-11-15 22:55:39 -0500

    tiles are in there space

--- myScript.js
+++ myScript.js
@@ -1,37 +1,19 @@
+
+const userName = prompt(`Welcome to Memory Game, please enter your name`);
+
+
+const cake = document.querySelector('.card');
+cake.addEventListener('click', function () {
+    cake.classList.toggle('is-flipped');
+});
+
 // Welcome Memory Game
 // Prompt username request
-const userName = prompt(`Welcome to Memory Game, please enter your username`);
 
-// Cards are to be shuffled on load or restart
-const card = document.querySelector('.card');
-card.addEventListener('click', function () {
-    card.classList.toggle('is-flipped');
-});
-
-const card1 = document.querySelector('.card1');
-card1.addEventListener('click', function () {
-    card1.classList.toggle('is-flipped');
-});
-
-const card2 = document.querySelector('.card2');
-card2.addEventListener('click', function () {
-    card2.classList.toggle('is-flipped');
-});
-
-const card3 = document.querySelector('.card3');
-card3.addEventListener('click', function () {
-    card3.classList.toggle('is-flipped');
-});
-
-// function getRandomInt(min, max) {
-//     return Math.floor(Math.random() * (max - min)) + min;
-// }
+// Define a card shuffle array
 
 
 // User will select one tile(listen for that button)
-
-
-
     //  Tile will flip and show image
 // User selects second tile
 // Tile will flip and show image
